ba5c049e87ede16c22d45f8af8e7b53b6606f3ee
Fallonious <FallonSkinner@gmail.com>
2015-03-11 16:25:58 -0400

    total time tracks for developers

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -14,12 +14,36 @@ class DevelopersController < ApplicationController
 
   def create
     @developer = Developer.new(developer_params)
+
+    respond_to do |format|
+      if @developer.save
+        format.html { redirect_to @developer, notice: 'developer was successfully created.' }
+        format.json { render :show, status: :created, location: @developer }
+      else
+        format.html { render :new }
+        format.json { render json: @developer.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
+    respond_to do |format|
+      if @developer.update(developer_params)
+        format.html { redirect_to @developer, notice: 'Bank account was successfully updated.' }
+        format.json { render :show, status: :ok, location: @developer }
+      else
+        format.html { render :edit }
+        format.json { render json: @developer.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @developer.destroy
+    respond_to do |format|
+      format.html { redirect_to developers_url, notice: 'developer was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   def edit
--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,4 +1,13 @@
 class Developer < ActiveRecord::Base
   has_many :time_entries
-  has_secure_password 
+  has_secure_password
+
+  def hours_worked
+    total_hours = 0.0
+    time_entries.each do |t|
+      total_hours += t.duration
+    end
+    return total_hours
+  end
+
 end
